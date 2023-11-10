cb79cf232f613fd2d92e42836d1b442d78fd005f
dothanhnhutis <dothanhnhutis@gmail.com>
2023-11-11 06:46:06 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
 export default function Home() {
-  return <div>sds</div>;
+  return (
+    <>
+      <header className="sticky top-0 z-50 w-full h-[70px] bg-background/95 backdrop-blur-[5px] backdrop-saturate-[1.8]">
+        <p className="text-black">ssds</p>
+      </header>
+      <main className="relative flex ">
+        <div className="sticky top-[70px] w-[220px] bg-red-500 h-[calc(100vh-70px)] flex flex-shrink-0">
+          dsda
+        </div>
+        <div className="max-w-7xl mx-auto">
+          <div className="bg-green-500 w-[1200px] h-[4000px]">
+            <p className=" font-bold text-4xl text-black">Header</p>
+          </div>
+        </div>
+      </main>
+      <footer className="bg-blue-500 h-[300px]">dsdads</footer>
+    </>
+  );
 }
